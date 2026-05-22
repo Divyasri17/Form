1eaa000cc702a6a9fb6c0bec7204afa8864a3379
divya <divyasripasupala17@gmail.com>
2026-05-22 11:47:39 +0530

    Updated form project

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function Form() {
+  const [data, setData] = useState({
+    name: "",
+    password: "",
+    email: "",
+    salary: 0,
+    roal: "",
+  });
+
+  const [val, setVal] = useState({
+    name: "",
+    password: "",
+    email: "",
+    salary: 0,
+    roal: "",
+  });
+  function handel(e) {
+    setData({ ...data, [e.target.name]: e.target.value });
+  }
+
+  function change(e) {
+    e.preventDefault();
+    setVal(data);
+    setData({
+      name: "",
+      password: "",
+      email: "",
+      salary: 0,
+      roal: "",
+    });
+  }
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <div>
+        <form>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name ..."
+            value={data.name}
+            onChange={handel}
+          />
+          <br />
+          <br />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password ..."
+            value={data.password}
+            onChange={handel}
+          />
+          <br />
+          <br />
+          <input
+            type="email"
+            name="email"
+            placeholder="email ..."
+            value={data.email}
+            onChange={handel}
+          />
+          <br />
+          <br />
+          <input
+            type="Number"
+            name="salary"
+            placeholder="salary ..."
+            value={data.salary}
+            onChange={handel}
+          />
+          <br />
+          <br />
+          <input
+            type="text"
+            name="roal"
+            placeholder="roal ..."
+            value={data.roal}
+            onChange={handel}
+          />
+          <button onClick={change}>Submit</button>
+        </form>
+        <h1>Name: {val.name}</h1>
+        <h1>Email: {val.email}</h1>
+        <h1>Password: {val.password}</h1>
+        <h1>Salary: {val.salary}</h1>
+        <h1>Roal: {val.roal}</h1>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export { Form };
